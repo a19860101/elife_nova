db2589114a7cea478bbc33eba52b609c85a9f979
Zac <a19860101@gmail.com>
2022-06-13 16:19:02 +0800

    20220613

--- app/Nova/Category.php
+++ app/Nova/Category.php
@@ -5,32 +5,23 @@ namespace App\Nova;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
-use Laravel\Nova\Fields\Textarea;
-use Laravel\Nova\Fields\Trix;
-use Laravel\Nova\Fields\Select;
-use Laravel\Nova\Fields\Boolean;
-use Laravel\Nova\Fields\Datetime;
-use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\Hidden;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
-use Illuminate\Support\Facades\Auth;
-
-class Post extends Resource
+class Category extends Resource
 {
     /**
      * The model the resource corresponds to.
      *
      * @var string
      */
-    public static $model = \App\Models\Post::class;
+    public static $model = \App\Models\Category::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
      *
      * @var string
      */
-    public static $title = 'id';
+    public static $title = 'title';
 
     /**
      * The columns that should be searched.
@@ -51,18 +42,7 @@ class Post extends Resource
     {
         return [
             ID::make()->sortable(),
-            Text::make('標題','title')->sortable()->placeholder('請輸入標題'),
-            Trix::make('內文','body')->withFiles('public'),
-            // Select::make('分類','category')->options([
-            //     'test' => '測試',
-            //     'tech' => '科技'
-            // ]),
-            Boolean::make('發布','is_published')->default(false),
-            Datetime::make('發布日期','publish_at')->nullable(),
-            BelongsTo::make('作者','user','App\Nova\User')->default(Auth::id())->exceptOnForms(),
-            Hidden::make('user_id')->default(Auth::id()),
-
-            BelongsTo::make('category')
+            Text::make('分類標題','title')
         ];
     }
 
