4c621cf5b7c6eeaee75aca7fc6d1ce4ec7266258
Zac <a19860101@gmail.com>
2022-06-17 16:11:30 +0800

    20220617

--- app/Nova/Actions/PublishPost.php
+++ app/Nova/Actions/PublishPost.php
@@ -29,23 +29,33 @@ class PublishPost extends Action
     public function handle(ActionFields $fields, Collection $models)
     {
         //
+        $users = \App\Models\User::all();
+
         foreach($models as $model){
             $model->update([
                 'is_published' => true
             ]);
+            foreach($users as $user){
+                $user->notify(
+                    NovaNotification::make()
+                        ->message('文章已發布')//右上小鈴鐺內訊息
+                        ->action('閱讀', URL::remote("http://localhost:8000/nova/resources/posts/{$model->id}"))
+                        // ->icon('download')
+                        ->type('info')
+                    );
+                }
         }
-        $users = \App\Models\User::all();
-        $post_id = $models-> pluck('id')->implode(',');
+        // $post_id = $models-> pluck('id')->implode(',');
 
-        foreach($users as $user){
-            $user->notify(
-                NovaNotification::make()
-                    ->message('文章已發布')//右上小鈴鐺內訊息
-                    ->action('閱讀', URL::remote("http://localhost:8000/nova/resources/posts/{$post_id}"))
-                    // ->icon('download')
-                    ->type('info')
-                );
-        }
+        // foreach($users as $user){
+        //     $user->notify(
+        //         NovaNotification::make()
+        //             ->message('文章已發布')//右上小鈴鐺內訊息
+        //             ->action('閱讀', URL::remote("http://localhost:8000/nova/resources/posts/{$post_id}"))
+        //             // ->icon('download')
+        //             ->type('info')
+        //         );
+        // }
 
     }
 
